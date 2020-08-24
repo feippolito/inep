4acd7db9c1990196929fa2ec70cabf50086d5984
Felipe Ippolito <felipeippolito@gmail.com>
2020-08-24 00:00:08 +0000

    move downloaded .zip files

--- src/data-pipeline/download-inep.py
+++ src/data-pipeline/download-inep.py
@@ -7,8 +7,13 @@ except Exception as e:
   raise ValueError('Pass folder to save as parameter')
 
 with open(os.path.join('data/links',f'{subset}.txt'),'r') as f:
-    lines = f.readlines()
+  lines = f.readlines()
+
+if not os.path.exists(f'./data/zipped/{subset}'):
+  os.mkdir(f'./data/zipped/{subset}')
 
 for line in lines:
-    command = f'wget {line} -P ./data/zipped/{subset}'
-    os.system(command)
+  command = f'wget {line} -P ./data/zipped/{subset}'
+  os.system(command)
+
+os.system(f'mv *.zip ./data/zipped/{subset}')
